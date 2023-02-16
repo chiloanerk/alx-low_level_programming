73829579b6bd8ca55df3638fb1a416d4e75ca57c
chiloanerk <chiloane.rk@gmail.com>
2023-02-16 14:51:30 -0800

    testing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /*
- * main - Prints a string without using printf and puts
- * Return: 0
+ * main - This program outputs a string without using fwrite.
  *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	fwrite(str1, 1, 59, stderr);
 	return (0);
 }
