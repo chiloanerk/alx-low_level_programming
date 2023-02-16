4e74d6e7a5f6dc379e7bde068751cd1d663cbdc0
chiloanerk <chiloane.rk@gmail.com>
2023-02-16 14:44:06 -0800

    test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(str1, 1, sizeof(str1), stderr);
+	fwrite(str1, 1, 59, stderr);
 	return (0);
 }
