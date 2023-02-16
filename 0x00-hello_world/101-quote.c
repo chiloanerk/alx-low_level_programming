c151156f276542117af97d8d3e1ee18f62769cbc
chiloanerk <chiloane.rk@gmail.com>
2023-02-16 14:38:36 -0800

    test

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Prints a string without using printf and puts
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(str1, 1, sizeof(str1), stderr);
+	return (0);
+}
