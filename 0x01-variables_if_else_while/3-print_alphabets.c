996d991040ee394ca94410ebffd882b5d1f23cd0
chiloanerk <chiloane.rk@gmail.com>
2023-02-17 15:00:00 -0800

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char az, AZ;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	for (AZ = 'A'; AZ <= 'Z'; AZ++)
+	{
+		putchar(AZ);
+	}
+	putchar('\n');
+	return (0);
+}
