f5285c38c30b49fb4c54626da6f97a032394dafc
chiloanerk <chiloane.rk@gmail.com>
2023-02-17 15:57:48 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
