288a27830b86711327f78529661d000aaaaf7621
chiloanerk <chiloane.rk@gmail.com>
2023-02-19 05:53:26 -0800

    The success combination in business is: Do what you do better... and: do more of what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,44 +1,39 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
-(*
+ * main - Program prints all possible diffrent combinations of three digits.
+ *
  * Return: 0
  */
 
-int	main(void)
+int main(void)
 {
-	int	i;
-	int	j;
-	int	k;
+	int i = '0';
+	int j = '0';
+	int k = '0';
 
-	i = '0';
-	j = '0';
-	k = '0';
 	while (i <= '7')
 	{
 		while (j <= '8')
 		{
+			while (k <= '9')
 			{
-				while (k <= '9')
+				if (i < j && j < k)
 				{
-					if (i < j && j < k)
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == '7' && j == '8' && k == '9')
 					{
-						putchar(i);
-						putchar(j);
-						putchar(k);
-						if (i == '7' && j == '8' && k == '9')
-						{
-							putchar('\n');
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar('\n');
 					}
-					k++;
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+				k++;
 			}
 			k = '0';
 			j++;
@@ -46,6 +41,6 @@ int	main(void)
 		j = '0';
 		i++;
 	}
+
 	return (0);
 }
-
