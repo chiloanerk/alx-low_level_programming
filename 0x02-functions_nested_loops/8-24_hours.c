22c9356ed91231df3b525d7573349b278aec9072
chiloanerk <chiloane.rk@gmail.com>
2023-02-21 14:15:35 -0800

    24 Hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return 0
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+	int minute = 0;
+
+	while (hour < 24)
+	{
+		while (minute < 60)
+		{
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
+			minute++;
+		}
+		minute = 0;
+		hour++;
+	}
+}
