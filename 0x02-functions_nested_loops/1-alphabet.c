9696f421e2db17ef57b3777d68644916b2a86012
chiloanerk <chiloane.rk@gmail.com>
2023-02-21 11:43:15 -0800

    removed whitespace for betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * main - funciton that prints the alphabet in lower case
+ * print_alphabet - funciton that prints the alphabet in lower case
  * Return: void
  */
 void print_alphabet(void)
 {
 	char c = 'a';
-	
+
 	while (c <= 'z')
 	{
 		_putchar(c);
