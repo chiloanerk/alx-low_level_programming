f90de687d73065b81fd687ef3d279f60fa95b583
chiloanerk <chiloane.rk@gmail.com>
2023-02-22 14:15:09 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @n: value to be checked
+ * @m: value to be checked
+ * Return: o value
+ */
+int add(int n, int m)
+{
+	int 0;
+	o = n + m;
+
+	return (o);
+}
+
