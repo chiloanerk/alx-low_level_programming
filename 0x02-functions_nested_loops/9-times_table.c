f63f4c577eac5e91a0f12c403b548de76f5c92fb
chiloanerk <chiloane.rk@gmail.com>
2023-02-22 13:21:01 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,15 +14,15 @@ void times_table(void)
 		{
 			int k = i * j;
 
-			if (j == 0)
+			if (j == 0 || i == 0)
 			{
-				_putchar('0');
 				_putchar(' ');
+				_putchar(k + '0');
 			}
 			else if (k < 10)
 			{
-				_putchar(k + '0');
 				_putchar(' ');
+				_putchar(k + '0');
 			}
 			else
 			{
