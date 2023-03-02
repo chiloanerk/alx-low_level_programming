f562e1095b52a098b8a7c6775d8c8b331ae9357c
chiloanerk <chiloane.rk@gmail.com>
2023-03-02 12:42:43 -0800

    seperators

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - captalises all words of a string
+ * @str: input pointer var
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	char seperators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}'};
+	int i , j, sepLen;
+
+	sepLen = 13;
+	j = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (j < sepLen)
+		{
+			if ((str[j - 1] == seperators[j] || j == 0) &&
+					(str[j] >= 97 && str[j] <= 122))
+			{
+				str[j] = str[j] - 32;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
