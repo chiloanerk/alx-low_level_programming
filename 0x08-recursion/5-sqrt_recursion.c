bd13c8eae55658129ca82704c686189631e09782
chiloanerk <chiloane.rk@gmail.com>
2023-03-08 12:31:41 -0800

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+int find_sqrt_helper(int n, int low, int high);
+/**
+ * _sqrt_recursion - Finds the square root of a number using recursion
+ * @n: the number to find the square root of
+ *
+ * Return: the square root of n, or -1 if n is negative
+ */
+int _sqrt_recursion(int n)
+{
+	return (find_sqrt_helper(n, 0, n));
+}
+
+/**
+ * find_sqrt_helper - Recursive helper function to find sqare root of a number
+ * @n: the number to find the square root of
+ * @low: the lower bound of the search
+ * @high: the upper bound
+ *
+ * Return: the square root of n if found, or -1 if not found
+ */
+
+int find_sqrt_helper(int n, int low, int high)
+{
+	int mid;
+
+	if (low > high)
+		return (-1);
+	mid = (low + high) / 2;
+
+	if (mid * mid == n)
+		return (mid);
+	else if (mid * mid > n)
+		return (find_sqrt_helper(n, low, mid - 1));
+	else
+		return (find_sqrt_helper(n, mid + 1, high));
+}
